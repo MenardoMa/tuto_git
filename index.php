d3855ce5c7d1d71b36f501a9e4c64cd22137ac96
Menardo tera <menardotera@gmail.com>
2023-04-13 16:13:59 +0200

    demo sur la versionning 1

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>versinning</title>
+</head>
+<body>
+    
+    <h1> versionning 1</h1>
+
+    <h1>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est ab 
+        officiis doloribus suscipit quaerat nesciunt quis pariatur adipisci consequatur 
+        deleniti nulla molestias aliquam, voluptatem ea optio! Pariatur, dolores qui. Voluptate.
+    </h1>
+
+</body>
+</html>
